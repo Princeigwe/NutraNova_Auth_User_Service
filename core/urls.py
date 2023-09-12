ea65cd73167eacebfef74cfd3c581a37c9438333
Prince Igwe <igwep297@gmail.com>
2023-09-12 04:11:07 +0100

    setup graphQL API... hello world

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from ariadne_django.views import GraphQLView
 
+from .schema import schema
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql/', GraphQLView.as_view(schema=schema), name='graphql'),
 ]
