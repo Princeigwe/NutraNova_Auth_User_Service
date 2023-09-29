d8a57664c3133602ed1b075a39a385100943ab67
Prince Igwe <igwep297@gmail.com>
2023-09-29 13:16:26 +0100

    got oidc user info

--- oidc/views.py
+++ oidc/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 import requests
 from dotenv import load_dotenv
 import os
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import json
 
 auth0_domain = os.environ.get("AUTH0_DOMAIN")
 client_id = os.environ.get('AUTH0_CLIENT_ID')
@@ -39,4 +40,17 @@ def oidc_callback(request):
     token_response = requests.post(token_url, data=token_data)
     token_response_data = token_response.json()
     print(token_response_data)
-    return HttpResponse("Authentication complete!")
+    # data = json.loads(token_response_data)
+    # return HttpResponse("Authentication complete!")
+    # return JsonResponse(token_response_data)
+
+    access_token = token_response_data['access_token']
+    user_info_url = f'https://{auth0_domain}/userinfo'
+
+    params = {
+        "access_token": access_token
+    }
+
+    response = requests.get(user_info_url, params=params)
+    user_info_response_data = response.json()
+    return JsonResponse(user_info_response_data)
