0366d41df6aa73c66ae2a6905bd9082d4979edc4
Prince Igwe <igwep297@gmail.com>
2023-10-01 02:08:55 +0100

    still debugging logical error in vercel cloud

--- oidc/urls.py
+++ oidc/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import oidc_authenticate, oidc_callback
 
 urlpatterns = [
-  path('auth/', oidc_authenticate),
+  path('auth/', oidc_authenticate, name="oidc_authenticate"),
   path('callback/', oidc_callback)
 ]
