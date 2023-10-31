77540c24f27bd9f8d0890828a9e6508710395be6
Prince Igwe <igwep297@gmail.com>
2023-10-31 01:06:40 +0100

    little change to onboard user resolver function

--- users/resolvers.py
+++ users/resolvers.py
@@ -8,12 +8,13 @@ User = get_user_model()
 @database_sync_to_async
 def resolve_onboardUser(_, info, input:dict):
 
-  request = info.context["request"] # get http request from info.context
-  authorization_header = request.headers.get("Authorization") # retrieve Authorization header to fetch value
-  parts = authorization_header.split(" ") # split value by the space
-  token = parts[1] # get the token
-  decoded_data = decode_access_token(token) # decode token
-  user_email = decoded_data['email']
+  # request = info.context["request"] # get http request from info.context
+  # authorization_header = request.headers.get("Authorization") # retrieve Authorization header to fetch value
+  # parts = authorization_header.split(" ") # split value by the space
+  # token = parts[1] # get the token
+  # decoded_data = decode_access_token(token) # decode token
+  # user_email = decoded_data['email']
+  user_email = get_user_email(info)
 
   if input['role'] == "USER" and ('specialization' in input):
     raise Exception("Invalid action: Cannot set specialization with USER role.")
