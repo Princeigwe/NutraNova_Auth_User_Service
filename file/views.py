a3b472ae6a90fafe8b1a3bee7fd46dcc0b138d1b
Prince Igwe <igwep297@gmail.com>
2023-11-03 21:23:01 +0100

    checked for spaces in file name

--- file/views.py
+++ file/views.py
@@ -14,7 +14,7 @@ import cloudinary
 import requests
 from django.core.files .storage import FileSystemStorage
 from django.conf import settings
-from pathlib import Path
+from rest_framework.exceptions import ParseError
 
 # Create your views here.
 
@@ -28,6 +28,11 @@ def upload_image_to_cloudinary(request):
   print(current_directory)
 
   if request_file:
+
+    # cloudinary does not work properly with files with gaps in it. example, default screenshots names 
+    if any( char.isspace() for char in request_file.name): # checking for gaps in the file name
+      raise ParseError("Image name cannot be parsed. Rename it or choose another")
+
     fs = FileSystemStorage()
     file = fs.save(request_file.name, request_file)
     file_url = fs.url(file)
