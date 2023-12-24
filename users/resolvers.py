48fd76a9d31cc8c126cd5e174fe8484c3e86e56c
Prince Igwe <igwep297@gmail.com>
2023-12-24 23:28:33 +0100

    on updating username, a new jwt is created in order for the user to interact properly with their created recipes in the recipes service

--- users/resolvers.py
+++ users/resolvers.py
@@ -4,6 +4,8 @@ from django.contrib.auth import get_user_model
 from utils.get_token_email import get_user_email
 from .models import UserFollowing
 from utils.kafka.produce.update_username_chef import send_updated_username
+from utils.jwt_encode_decode import encode_access_token
+
 
 User = get_user_model()
 
@@ -110,8 +112,32 @@ def resolve_update_username(_, info, input:dict):
       "old_username": old_username,
       "new_username": user.username # updated username
     }
+
     send_updated_username(kafka_message)
-    return user
+
+    # create new access token for user of updated username
+    payload = {
+        "username": user.username,
+        "email": user.email,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "age": user.age,
+        "gender": user.gender,
+        "role": user.role,
+        "dietary_preference": user.dietary_preference,
+        "taste_preferences": user.taste_preferences,
+        "health_goal": user.health_goal,
+        "allergens": user.allergens,
+        "activity_level": user.activity_level,
+        "cuisines": user.cuisines,
+        "medical_conditions": user.medical_conditions,
+        "is_on_boarded": user.is_on_boarded,
+    }
+    jwt = encode_access_token(payload)
+    return {
+      "user": user, 
+      "jwt": jwt
+    }
   except User.DoesNotExist:
     raise Exception("User does not exist")
 
