c57b5b948bad90c1261d7d2e9edd33e5ad6514b8
Prince Igwe <igwep297@gmail.com>
2024-02-19 17:15:57 +0100

    added render host to allowed host

--- core/settings.py
+++ core/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['nutranova-user.onrender.com', '127.0.0.1', 'localhost']
 ENVIRONMENT = os.environ.get('ENVIRONMENT')
 
 
