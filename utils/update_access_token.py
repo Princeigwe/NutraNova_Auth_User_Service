eeca526df55d1f82047e85ff1a4b3292c7645a72
Prince Igwe <igwep297@gmail.com>
2024-04-26 20:18:18 +0100

    code refactor for onboarding user response

---
+++ utils/update_access_token.py
@@ -0,0 +1,24 @@
+from .jwt_encode_decode import encode_access_token
+
+
+def update_access_token(user):
+  """this is used to update the access token for a user for every change in profile data"""
+  payload = {
+    "username": user.username,
+    "email": user.email,
+    "first_name": user.first_name,
+    "last_name": user.last_name,
+    "age": user.age,
+    "gender": user.gender,
+    "role": user.role,
+    "dietary_preference": user.dietary_preference,
+    "taste_preferences": user.taste_preferences,
+    "health_goal": user.health_goal,
+    "allergens": user.allergens,
+    "activity_level": user.activity_level,
+    "cuisines": user.cuisines,
+    "medical_conditions": user.medical_conditions,
+    "is_on_boarded": user.is_on_boarded,
+    }
+  jwt = encode_access_token(payload)
+  return jwt
