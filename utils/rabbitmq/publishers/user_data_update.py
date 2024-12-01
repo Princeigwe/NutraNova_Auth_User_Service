04a8be4bdaa3634bc8102f1de28e4abd00ab9029
Prince Igwe <igwep297@gmail.com>
2024-12-01 18:21:05 +0100

    rabbitmq mesesage publication

--- utils/rabbitmq/publishers/user_data_update.py
+++ utils/rabbitmq/publishers/user_data_update.py
@@ -1,9 +1,17 @@
 import pika, os, logging
 from pika.exceptions import AMQPError
 import json
+import ssl
+
+# i was having an issue with an SSLEOFerror. 
+ssl_context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+ssl_context.minimum_version = ssl.TLSVersion.TLSv1_2
 
 url = os.environ.get('CLOUDAMQP_URL')
 params = pika.URLParameters(url)
+
+# i was having an issue with an SSLEOFerror. 
+params.ssl_options=pika.SSLOptions(context=ssl_context) 
 
 user_data_update_queue = os.environ.get('CLOUDAMQP_USER_DATA_UPDATE_QUEUE')
 
@@ -15,6 +23,6 @@ def send_user_data_update(message: dict):
   try:
     channel.basic_publish(exchange='', routing_key=user_data_update_queue, body=json.dumps(message))
     print ("cloudamqp: Message sent to consumer")
-    connection.close()
+    # connection.close()
   except AMQPError as e:
     logging.exception(e)
