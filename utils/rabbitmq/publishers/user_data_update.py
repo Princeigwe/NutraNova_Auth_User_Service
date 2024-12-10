0a9c9b1d198905af790f9739e8d36ec81bc3eedb
Prince Igwe <igwep297@gmail.com>
2024-12-10 17:45:11 +0100

    little config change

--- utils/rabbitmq/publishers/user_data_update.py
+++ utils/rabbitmq/publishers/user_data_update.py
@@ -9,6 +9,7 @@ ssl_context.minimum_version = ssl.TLSVersion.TLSv1_2
 
 url = os.environ.get('CLOUDAMQP_URL')
 params = pika.URLParameters(url)
+params.heartbeat = 0 # setting heartbeat to zero to stop heartbeat timeout error
 
 # i was having an issue with an SSLEOFerror. 
 params.ssl_options=pika.SSLOptions(context=ssl_context) 
@@ -17,7 +18,7 @@ user_data_update_queue = os.environ.get('CLOUDAMQP_USER_DATA_UPDATE_QUEUE')
 
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
-channel.queue_declare(user_data_update_queue)
+channel.queue_declare(user_data_update_queue, durable=True) # "durable" to match the durable config in the AMQP server, as the queue was setup manually on the server and not the code.
 
 def send_user_data_update(message: dict):
   try:
