fa015cd90fa4fc286a4f13243cf69e8c27c76835
Prince Igwe <igwep297@gmail.com>
2024-12-10 20:33:22 +0100

    changes to rabbitmq publisher

--- users/tasks.py
+++ users/tasks.py
@@ -2,24 +2,25 @@ from celery import shared_task
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.response import Response
-from utils.kafka.produce.user_data_update import send_user_data_update
+# from utils.kafka.produce.user_data_update import send_user_data_update
 
-
-User = get_user_model()
+## todo: work on this feature for rabbitmq
+# User = get_user_model()
 
 @shared_task
 def set_profile_image(image_url, user_email):
-  try:
-    user = User.objects.get(email=user_email)
-    user.image = image_url
-    user.save()
-    # return user
-    print("user image updated")
-    kafka_message = {
-      "username": user.username,
-      "image": user.image,
-    }
-    send_user_data_update(kafka_message)
-    print("kafka message sent...")
-  except User.DoesNotExist:
-    return Response({"message": "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
+  # try:
+  #   user = User.objects.get(email=user_email)
+  #   user.image = image_url
+  #   user.save()
+  #   # return user
+  #   print("user image updated")
+  #   kafka_message = {
+  #     "username": user.username,
+  #     "image": user.image,
+  #   }
+  #   send_user_data_update(kafka_message)
+  #   print("kafka message sent...")
+  # except User.DoesNotExist:
+  #   return Response({"message": "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
+  pass
--- utils/rabbitmq/publishers/user_data_update.py
+++ utils/rabbitmq/publishers/user_data_update.py
@@ -13,16 +13,22 @@ params.heartbeat = 0 # setting heartbeat to zero to stop heartbeat timeout error
 
 # i was having an issue with an SSLEOFerror. 
 params.ssl_options=pika.SSLOptions(context=ssl_context) 
+exchange_name=os.environ.get('CLOUDAMQP_FANOUT_EXCHANGE')
 
 user_data_update_queue = os.environ.get('CLOUDAMQP_USER_DATA_UPDATE_QUEUE')
 
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
-channel.queue_declare(user_data_update_queue, durable=True) # "durable" to match the durable config in the AMQP server, as the queue was setup manually on the server and not the code.
+
+## creation of fanout exchange
+channel.exchange_declare(exchange=exchange_name, exchange_type='fanout')
+
+# channel.queue_declare(user_data_update_queue, durable=True) # "durable" to match the durable config in the AMQP server, as the queue was setup manually on the server and not the code.
 
 def send_user_data_update(message: dict):
   try:
-    channel.basic_publish(exchange='', routing_key=user_data_update_queue, body=json.dumps(message))
+    # channel.basic_publish(exchange='', routing_key=user_data_update_queue, body=json.dumps(message))
+    channel.basic_publish(exchange=exchange_name, routing_key='', body=json.dumps(message)) # publishing to fanout exchange
     print ("cloudamqp: Message sent to consumer")
     # connection.close()
   except AMQPError as e:
