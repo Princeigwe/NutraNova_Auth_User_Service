8f8e1b6a94f757dd814a177cd0c4a7ce86a31c91
Prince Igwe <igwep297@gmail.com>
2025-01-04 12:21:44 +0100

    maximum age of messages

--- utils/rabbitmq/stream.py
+++ utils/rabbitmq/stream.py
@@ -3,4 +3,4 @@ from .channel import channel
 
 # stream declaration
 stream_name=os.environ.get('RABBITMQ_STREAM')
-channel.queue_declare(queue=stream_name, durable=True, arguments={"x-queue-type": "stream"})
+channel.queue_declare(queue=stream_name, durable=True, arguments={"x-queue-type": "stream", "x-max-age": "7D"}) ## message in the stream queue is set to delete after 7 days
