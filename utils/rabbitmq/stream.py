a10091b296cdde516a760c557d4f20f3f2be319f
Prince Igwe <igwep297@gmail.com>
2025-01-10 22:59:37 +0100

    updated message queue limit and expiration time for messages

--- utils/rabbitmq/stream.py
+++ utils/rabbitmq/stream.py
@@ -3,4 +3,4 @@ from .channel import channel
 
 # stream declaration
 stream_name=os.environ.get('RABBITMQ_STREAM')
-channel.queue_declare(queue=stream_name, durable=True, arguments={"x-queue-type": "stream", "x-max-age": "7D"}) ## message in the stream queue is set to delete after 7 days
+channel.queue_declare(queue=stream_name, durable=True, arguments={"x-queue-type": "stream", "x-max-age": "3D", "x-max-length": 56}) ## message in the stream queue is set to delete after 7 days
