c58ba4436dcb804b51aa84fa8ad7e3eb93fde040
Prince Igwe <igwep297@gmail.com>
2025-01-22 18:57:38 +0100

    changes in retention policies of declared steram

--- utils/rabbitmq/stream.py
+++ utils/rabbitmq/stream.py
@@ -3,4 +3,9 @@ from .channel import channel
 
 # stream declaration
 stream_name=os.environ.get('RABBITMQ_STREAM')
-channel.queue_declare(queue=stream_name, durable=True, arguments={"x-queue-type": "stream", "x-max-age": "3D", "x-max-length": 56}) ## message in the stream queue is set to delete after 7 days
+channel.queue_declare(queue=stream_name, durable=True, arguments={
+  "x-queue-type": "stream", 
+  "x-max-age": "1D",
+  "x-max-length-bytes": 5000000, 
+  "x-stream-max-segment-size-bytes":5000
+  }) 
